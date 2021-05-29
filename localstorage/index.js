5662a5cf6de38f3e5bb79b6ab5458b55e2101b43
Bhoomit <bhumit070@gmail.com>
2021-05-29 15:45:10 +0530

    added localstorage set and get

--- localstorage/index.js
+++ localstorage/index.js
@@ -1,21 +1,21 @@
-const setItem = (key,value) => {
-    if(!item || !value || typeof window === undefined) {
-        return false
-    }
-    localStorage.setItem(key,JSON.stringify(value))
-    return true
+const setItem = (key, value) => {
+  if (!key || !value || typeof window === 'undefined') {
+    return false
+  }
+  localStorage.setItem(key, JSON.stringify(value))
+  return true
 }
 
 const getItem = (key) => {
-    if(!key) {
-        return false
-    }
-    const value = localStorage.getItem(key)
-    if(!value) return false
-    return JSON.parse(value)
+  if (!key || typeof window === 'undefined') {
+    return false
+  }
+  const value = localStorage.getItem(key)
+  if (!value) return false
+  return JSON.parse(value)
 }
 
 module.exports = {
-    setItem,
-    getItem
+  setItem,
+  getItem,
 }

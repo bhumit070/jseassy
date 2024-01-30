ea21df035705dbed888d568b6e396fa29f372709
bhumit070 <ganatrabhoomit070@gmail.com>
2024-01-30 22:34:13 +0530

    update: formdata.ts

--- src/formdata/utils.ts
+++ src/formdata/utils.ts
@@ -1,25 +1,30 @@
-const keys = (formdata) => {
-    const keys: string[] = [];
-    if (!(formdata instanceof FormData)) return keys;
-    // @ts-ignore
-    for (const key of formdata.keys()) {
-        keys.push(key);
+const isValidFormData = (formData: FormData) => {
+    if (!formData || !(formData instanceof FormData)) {
+        throw new Error(
+            'Invalid argument type. Expected an instance of FormData.'
+        );
     }
+};
+
+export const keys = (formdata: FormData) => {
+    isValidFormData(formdata);
+    const keys: string[] = [];
+    if (!(formdata instanceof FormData)) {
+        throw new Error(
+            'Invalid argument type. Expected an instance of FormData.'
+        );
+    }
+    formdata.forEach((value, key) => {
+        keys.push(key);
+    });
     return keys;
 };
 
-const values = (formdata) => {
-    const values = [];
-    if (!(formdata instanceof FormData)) return values;
-    // @ts-ignore
-    for (const value of formdata.values()) {
-        // @ts-ignore
+export const values = (formdata: FormData): Array<FormDataEntryValue> => {
+    isValidFormData(formdata);
+    const values: Array<FormDataEntryValue> = [];
+    formdata.forEach((value) => {
         values.push(value);
-    }
+    });
     return values;
-};
-
-module.exports = {
-    keys,
-    values,
 };

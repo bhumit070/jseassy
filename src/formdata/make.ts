36a5ebaba94ece935269c581bdf5a7c8735f54a5
bhumit070 <ganatrabhoomit070@gmail.com>
2024-01-30 22:41:36 +0530

    update: formdata/make.ts

--- src/formdata/make.ts
+++ src/formdata/make.ts
@@ -1,32 +1,27 @@
-const convertToFormData = (data) => {
-    if (Array.isArray(data)) return convertArrayToFormData(data);
-    if (!Array.isArray(data) && typeof data === 'object')
-        return convertObjectToFormData(data);
-    return false;
+const convertToFormData = (data: unknown) => {
+    if (data) {
+        if (Array.isArray(data)) {
+            return convertArrayToFormData(data);
+        }
+        if (!Array.isArray(data) && typeof data === 'object') {
+            return convertObjectToFormData(data);
+        }
+    }
+    throw new Error('Invalid argument type. Expected an object or array.');
 };
 
-const convertObjectToFormData = (data) => {
+const convertObjectToFormData = <T extends object>(data: T) => {
     const fd = new FormData();
-    for (const value in data) {
-        // @ts-ignore
-        fd.append(value, object[value]);
+    for (const key in data) {
+        fd.append(key as never, data[key] as FormDataEntryValue);
     }
     return fd;
 };
 
-const convertArrayToFormData = (data) => {
+const convertArrayToFormData = (data: Array<FormDataEntryValue>) => {
     const fd = new FormData();
     data.map((value, index) => {
-        if (typeof value === 'object') {
-            const key = Object.keys(value)[0];
-            // @ts-ignore
-            fd.append(key, value[key]);
-        } else {
-            // @ts-ignore
-            fd.append(index, value);
-        }
+        fd.append(index as never, value);
     });
     return fd;
 };
-
-module.exports = convertToFormData;

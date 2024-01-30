bf26f16cf3e82fc8f7be6c64da2d91ff9ee4eabd
bhumit070 <ganatrabhoomit070@gmail.com>
2024-01-30 21:47:02 +0530

    add: remove.ts

--- src/array/remove.ts
+++ src/array/remove.ts
@@ -1,9 +1,20 @@
-export const remove = (arr, value) => {
+export const remove = <TArray extends Array<any>, TValue = unknown>(
+    arr: TArray,
+    value: TValue
+) => {
     if (!Array.isArray(arr)) return arr;
     return arr.filter((num) => num !== value);
 };
 
-export const removeByKey = (arr, key, value) => {
+export const removeByKey = <
+    TArray extends Array<Record<string, unknown>>,
+    TKey extends keyof TArray[number] extends string ? string : never,
+    TValue,
+>(
+    arr: TArray,
+    key: TKey,
+    value: unknown
+) => {
     if (!key || !Array.isArray(arr) || !value) return arr;
     return arr.filter((obj) => obj[key] !== value);
 };

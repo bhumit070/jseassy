586b8b5a35964ecbc7ddb8a19855c1897d1b18ba
bhumit070 <ganatrabhoomit070@gmail.com>
2024-01-30 22:14:31 +0530

    update: obj helpers

--- src/objects/freeze.ts
+++ src/objects/freeze.ts
@@ -1,6 +1,6 @@
-const freeze = (obj) => {
-  if (typeof obj !== 'object') return 0
-  return Object.freeze(obj)
-}
-
-module.exports = freeze
+export const freeze = (obj: object) => {
+    if (typeof obj !== 'object') {
+        throw new Error('Invalid argument type. Expected an object.');
+    }
+    return Object.freeze(obj);
+};
--- src/objects/length.ts
+++ src/objects/length.ts
@@ -1,5 +1,7 @@
-export const length = (obj) => {
-    if (typeof obj !== 'object') return 0;
+export const length = (obj: object) => {
+    if (typeof obj !== 'object' || Array.isArray(obj)) {
+        throw new Error('Invalid argument type. Expected an object.');
+    }
     const keysLength = Object.keys(obj).length;
     const symbolicKeysLength = Object.getOwnPropertySymbols(obj).length;
     return keysLength + symbolicKeysLength;

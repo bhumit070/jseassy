f73d63a2ef14dffe11ba627eeaf8faa980de0a29
bhumit070 <ganatrabhoomit070@gmail.com>
2024-01-30 21:50:22 +0530

    add: sortByKey.ts

--- src/array/sortByKey.ts
+++ src/array/sortByKey.ts
@@ -1,22 +1,25 @@
-const sortAscByKey = (arr, key) => {
-  if (!Array.isArray(arr)) return arr
-  return arr.sort((a, b) => {
-    if (a[key] > b[key]) return 1
-    else if (a[key] < b[key]) return -1
-    else return 0
-  })
-}
+export const sortAscByKey = <T extends Record<string, unknown>>(
+    arr: Array<T>,
+    key: keyof T
+) => {
+    if (!Array.isArray(arr)) {
+        return arr;
+    }
+    return arr.sort((a, b) => {
+        if (a[key] > b[key]) return 1;
+        else if (a[key] < b[key]) return -1;
+        else return 0;
+    });
+};
 
-const sortDescByKey = (arr, key) => {
-  if (!Array.isArray(arr)) return arr
-  return arr.sort((a, b) => {
-    if (a[key] > b[key]) return -1
-    else if (a[key] < b[key]) return 1
-    else return 0
-  })
-}
-
-module.exports = {
-  sortAscByKey,
-  sortDescByKey,
-}
+export const sortDescByKey = <T extends Record<string, unknown>>(
+    arr: Array<T>,
+    key: keyof T
+) => {
+    if (!Array.isArray(arr)) return arr;
+    return arr.sort((a, b) => {
+        if (a[key] > b[key]) return -1;
+        else if (a[key] < b[key]) return 1;
+        else return 0;
+    });
+};
